5bb1b627f0b49828e73c4900aabee6dd0cdb9bf3
tropicahq <tropicahq@gmail.com>
2025-11-10 12:52:17 +0100

    doing check ups

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,7 +1,9 @@
+import "dotenv/config"
 import { drizzle } from "drizzle-orm/neon-http"
 import { neon } from "@neondatabase/serverless"
 import * as schema from '~/db/schema'
 import * as betterAuthSchema from "../../auth-schema"
 
+console.log(JSON.stringify(process.env))
 export const sql = neon(process.env.DATABASE_URL!)
 export const db = drizzle({ client: sql, schema: { ...schema, ...betterAuthSchema } })
